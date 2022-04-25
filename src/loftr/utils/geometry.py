3a4a7315392d4e60a3638e9fae84d323f75c2735
sublatelog <blueganz@gmail.com>
2022-04-25 23:05:28 +0900

    Update geometry.py

--- src/loftr/utils/geometry.py
+++ src/loftr/utils/geometry.py
@@ -9,7 +9,7 @@ def warp_kpts(kpts0, depth0, depth1, T_0to1, K0, K1):
     Depth is consistent if relative error < 0.2 (hard-coded).
     
     Args:
-        kpts0 (torch.Tensor): [N, L, 2] - <x, y>,
+        kpts0 (torch.Tensor): [N, L, 2] - <x, y>, 格子座標
         depth0 (torch.Tensor): [N, H, W],
         depth1 (torch.Tensor): [N, H, W],
         T_0to1 (torch.Tensor): [N, 3, 4],
@@ -19,17 +19,21 @@ def warp_kpts(kpts0, depth0, depth1, T_0to1, K0, K1):
         calculable_mask (torch.Tensor): [N, L]
         warped_keypoints0 (torch.Tensor): [N, L, 2] <x0_hat, y1_hat>
     """
-    kpts0_long = kpts0.round().long()
+    kpts0_long = kpts0.round().long() # 四捨五入
 
-    # Sample depth, get calculable_mask on depth != 0
+    # depth x 格子座標,　Sample depth, get calculable_mask on depth != 0
     kpts0_depth = torch.stack(
-        [depth0[i, kpts0_long[i, :, 1], kpts0_long[i, :, 0]] for i in range(kpts0.shape[0])], dim=0
-    )  # (N, L)
-    nonzero_mask = kpts0_depth != 0
+                              [depth0[i, kpts0_long[i, :, 1], kpts0_long[i, :, 0]] for i in range(kpts0.shape[0])], 
+                              dim=0
+                              )  # (N, L)
+    
+    nonzero_mask = kpts0_depth != 0 # ゼロ以外を取り出す
 
     # Unproject
-    kpts0_h = torch.cat([kpts0, torch.ones_like(kpts0[:, :, [0]])], dim=-1) * kpts0_depth[..., None]  # (N, L, 3)
-    kpts0_cam = K0.inverse() @ kpts0_h.transpose(2, 1)  # (N, 3, L)
+    kpts0_h = torch.cat([kpts0, torch.ones_like(kpts0[:, :, [0]])], dim=-1) * kpts0_depth[..., None]  # (N, L, 3) # 同じサイズで値が1のベクトル
+    
+    kpts0_cam = K0.inverse() @ kpts0_h.transpose(2, 1)  # (N, 3, L) # [@]:行列積
+    
 
     # Rigid Transform
     w_kpts0_cam = T_0to1[:, :3, :3] @ kpts0_cam + T_0to1[:, :3, [3]]    # (N, 3, L)
@@ -41,14 +45,15 @@ def warp_kpts(kpts0, depth0, depth1, T_0to1, K0, K1):
 
     # Covisible Check
     h, w = depth1.shape[1:3]
-    covisible_mask = (w_kpts0[:, :, 0] > 0) * (w_kpts0[:, :, 0] < w-1) * \
-        (w_kpts0[:, :, 1] > 0) * (w_kpts0[:, :, 1] < h-1)
+    covisible_mask = (w_kpts0[:, :, 0] > 0) * (w_kpts0[:, :, 0] < w-1) * (w_kpts0[:, :, 1] > 0) * (w_kpts0[:, :, 1] < h-1)
     w_kpts0_long = w_kpts0.long()
     w_kpts0_long[~covisible_mask, :] = 0
 
     w_kpts0_depth = torch.stack(
-        [depth1[i, w_kpts0_long[i, :, 1], w_kpts0_long[i, :, 0]] for i in range(w_kpts0_long.shape[0])], dim=0
-    )  # (N, L)
+                                [depth1[i, w_kpts0_long[i, :, 1], w_kpts0_long[i, :, 0]] for i in range(w_kpts0_long.shape[0])], 
+                                dim=0
+                                )  # (N, L)
+    
     consistent_mask = ((w_kpts0_depth - w_kpts0_depth_computed) / w_kpts0_depth).abs() < 0.2
     valid_mask = nonzero_mask * covisible_mask * consistent_mask
 
