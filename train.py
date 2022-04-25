4ad21a902c07559b4edcef7bcb4fed94c123bfb7
sublatelog <blueganz@gmail.com>
2022-04-25 15:55:15 +0900

    Update train.py

--- train.py
+++ train.py
@@ -24,31 +24,38 @@ def parse_args():
     # init a costum parser which will be added into pl.Trainer parser
     # check documentation: https://pytorch-lightning.readthedocs.io/en/latest/common/trainer.html#trainer-flags
     parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-    parser.add_argument(
-        'data_cfg_path', type=str, help='data config path')
-    parser.add_argument(
-        'main_cfg_path', type=str, help='main config path')
-    parser.add_argument(
-        '--exp_name', type=str, default='default_exp_name')
-    parser.add_argument(
-        '--batch_size', type=int, default=4, help='batch_size per gpu')
-    parser.add_argument(
-        '--num_workers', type=int, default=4)
-    parser.add_argument(
-        '--pin_memory', type=lambda x: bool(strtobool(x)),
-        nargs='?', default=True, help='whether loading data to pinned memory or not')
-    parser.add_argument(
-        '--ckpt_path', type=str, default=None,
-        help='pretrained checkpoint path, helpful for using a pre-trained coarse-only LoFTR')
-    parser.add_argument(
-        '--disable_ckpt', action='store_true',
-        help='disable checkpoint saving (useful for debugging).')
-    parser.add_argument(
-        '--profiler_name', type=str, default=None,
-        help='options: [inference, pytorch], or leave it unset')
-    parser.add_argument(
-        '--parallel_load_data', action='store_true',
-        help='load datasets in with multiple processes.')
+    parser.add_argument('data_cfg_path', type=str, help='data config path')
+    parser.add_argument('main_cfg_path', type=str, help='main config path')
+    parser.add_argument('--exp_name', type=str, default='default_exp_name')
+    parser.add_argument('--batch_size', type=int, default=4, help='batch_size per gpu')
+    parser.add_argument('--num_workers', type=int, default=4)
+    parser.add_argument('--pin_memory', type=lambda x: bool(strtobool(x)), nargs='?', default=True, help='whether loading data to pinned memory or not')
+    parser.add_argument('--ckpt_path', type=str, default=None, help='pretrained checkpoint path, helpful for using a pre-trained coarse-only LoFTR')
+    parser.add_argument('--disable_ckpt', action='store_true', help='disable checkpoint saving (useful for debugging).')
+    parser.add_argument('--profiler_name', type=str, default=None, help='options: [inference, pytorch], or leave it unset')
+    parser.add_argument('--parallel_load_data', action='store_true', help='load datasets in with multiple processes.')
+    
+    """
+    reproduce_train/outdoor_ds.sh
+    python -u ./train.py \
+    configs/data/megadepth_trainval_640.py \
+    configs/loftr/outdoor/loftr_ds_dense.py \
+    --exp_name=${exp_name} \
+    --gpus=4 
+    --num_nodes=1 
+    --accelerator="ddp" \
+    --batch_size=1 
+    --num_workers=4 
+    --pin_memory=true \
+    --check_val_every_n_epoch=1 \
+    --log_every_n_steps=1 \
+    --flush_logs_every_n_steps=1 \
+    --limit_val_batches=1. \
+    --num_sanity_val_steps=10 \
+    --benchmark=True \
+    --max_epochs=30
+
+    """
 
     parser = pl.Trainer.add_argparse_args(parser)
     return parser.parse_args()
@@ -68,9 +75,11 @@ def main():
     # This is needed for data augmentation
     
     # scale lr and warmup-step automatically
-    args.gpus = _n_gpus = setup_gpus(args.gpus)
-    config.TRAINER.WORLD_SIZE = _n_gpus * args.num_nodes
-    config.TRAINER.TRUE_BATCH_SIZE = config.TRAINER.WORLD_SIZE * args.batch_size
+    
+    # confと環境に合わせてGPUを設定
+    args.gpus = _n_gpus = setup_gpus(args.gpus) # 4
+    config.TRAINER.WORLD_SIZE = _n_gpus * args.num_nodes # 4 * 1
+    config.TRAINER.TRUE_BATCH_SIZE = config.TRAINER.WORLD_SIZE * args.batch_size # 
     _scaling = config.TRAINER.TRUE_BATCH_SIZE / config.TRAINER.CANONICAL_BS
     config.TRAINER.SCALING = _scaling
     config.TRAINER.TRUE_LR = config.TRAINER.CANONICAL_LR * _scaling
@@ -91,31 +100,47 @@ def main():
     
     # Callbacks
     # TODO: update ModelCheckpoint to monitor multiple metrics
-    ckpt_callback = ModelCheckpoint(monitor='auc@10', verbose=True, save_top_k=5, mode='max',
+    
+    # Checkpoint
+    ckpt_callback = ModelCheckpoint(
+                                    monitor='auc@10', 
+                                    verbose=True, 
+                                    save_top_k=5, 
+                                    mode='max',
                                     save_last=True,
                                     dirpath=str(ckpt_dir),
-                                    filename='{epoch}-{auc@5:.3f}-{auc@10:.3f}-{auc@20:.3f}')
+                                    filename='{epoch}-{auc@5:.3f}-{auc@10:.3f}-{auc@20:.3f}'
+                                   )
+    
     lr_monitor = LearningRateMonitor(logging_interval='step')
+    
     callbacks = [lr_monitor]
     if not args.disable_ckpt:
         callbacks.append(ckpt_callback)
     
     # Lightning Trainer
     trainer = pl.Trainer.from_argparse_args(
-        args,
-        plugins=DDPPlugin(find_unused_parameters=False,
-                          num_nodes=args.num_nodes,
-                          sync_batchnorm=config.TRAINER.WORLD_SIZE > 0),
-        gradient_clip_val=config.TRAINER.GRADIENT_CLIPPING,
-        callbacks=callbacks,
-        logger=logger,
-        sync_batchnorm=config.TRAINER.WORLD_SIZE > 0,
-        replace_sampler_ddp=False,  # use custom sampler
-        reload_dataloaders_every_epoch=False,  # avoid repeated samples!
-        weights_summary='full',
-        profiler=profiler)
+                                            args,
+                                            plugins=DDPPlugin(
+                                                              find_unused_parameters=False,
+                                                              num_nodes=args.num_nodes,
+                                                              sync_batchnorm=config.TRAINER.WORLD_SIZE > 0
+                                                              ),
+                                            gradient_clip_val=config.TRAINER.GRADIENT_CLIPPING,
+                                            callbacks=callbacks,
+                                            logger=logger,
+                                            sync_batchnorm=config.TRAINER.WORLD_SIZE > 0,
+                                            replace_sampler_ddp=False,  # use custom sampler
+                                            reload_dataloaders_every_epoch=False,  # avoid repeated samples!
+                                            weights_summary='full',
+                                            profiler=profiler
+                                            )
+    
+    # log
     loguru_logger.info(f"Trainer initialized!")
     loguru_logger.info(f"Start training!")
+    
+    # fit
     trainer.fit(model, datamodule=data_module)
 
 
